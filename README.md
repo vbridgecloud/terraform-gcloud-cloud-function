84d38216e354ed5ceede1e4ffb381430002f1b29
Alex De Smet <alexander.desmet@vbridge.eu>
2020-01-03 10:31:26 +0100

    initial version resources

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # terraform-gcloud-pubsub-cloud-function
+
 Terraform deploy a local folder to a Google Cloud Function that can be triggered by a Pub/Sub subscription
+
+## Basic Usage
+
+```hcl
+# Create a Storage Bucket to store the Cloud Functions in
+resource "google_storage_bucket" "cloudfunctions_bucket" {
+  name     = "myproject-cloud-functions"
+  location = "europe-west3"
+}
+
+# Create a cloud function named `hello-world`
+module "cloudfunction--hello-world" {
+  source      = "github.com/vbridgebvba/terraform-gcloud-http-cloud-function"
+  bucket_name = google_storage_bucket.cloudfunctions_bucket.name
+  name        = "hello-world"
+}
+```
+
+The module `terraform-gcloud-http-cloud-function` will:
+
+1. Zip up the contents of the `./cloudfunctions/hello-world` folder
+2. Store the `hello-world.zip` file as an object into the bucket
+3. Create the Cloud Function, linking to the `hello-world.zip` object in the bucket
+
+## Variables
+
+### Required Variables
+
+- `bucket_name`: Name of GCS bucket to use to store the Cloud Functions their contents on.
+- `name`: Name of the cloud function
+
+### Optional Variables and their defaults
+
+- `source_dir`= `"./cloudfunctions/${var.name}"`
+- `description`: `"${var.name} HTTP Cloud Function"`
+- `runtime`: `"python37"`
+- `entrypoint`: `"__main__"`
+- `available_memory_mb`: `128`
+- `timeout`: `60`
+- `environment_variables`= `{}`
+- `service_account_email` = `""`
+- `vpc_connector` = `null`
+
+## Extended Example (Overriding the defaults)
+
+```hcl
+# Create a Storage Bucket to store the Cloud Functions in
+resource "google_storage_bucket" "cloudfunctions_bucket" {
+  name     = "myproject-cloud-functions"
+  location = "europe-west3"
+}
+
+# Create a cloud function named `hello-world`
+module "cloudfunction--hello-world" {
+  source                 = "github.com/vbridgebvba/terraform-gcloud-http-cloud-function"
+  bucket_name            = google_storage_bucket.cloudfunctions_bucket.name
+  name                   = "hello-world"
+  source_dir             = "./functions/hello-world/src"
+  runtime                = "nodejs10"
+  available_memory_mb    = 512
+  timeout                = 120
+  service_account_email  = "cloud-function-invoker@project.iam.gserviceaccount.com"
+  vpc_connector          = "vpc-access-connector-name"
+}
+```
+
+## License
+
+`terraform-gcloud-http-cloud-function` is released under the MIT License. See the enclosed [`LICENSE` file](LICENSE) for details.
---
+++ variables.tf
@@ -0,0 +1,84 @@
+## REQUIRED VARIABLES
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of GCS bucket to use to store the Cloud Functions their contents on."
+}
+
+variable "name" {
+  type        = string
+  description = "A user-defined name of the function. Function names must be unique globally."
+}
+
+## OPTIONAL VARIABLES
+
+variable "source_dir" {
+  type        = string
+  description = "Source directory containing the function source files. Its contents will be zipped and uploaded to the given `bucket_name`"
+  default     = "" # "./cloudfunctions/${var.name}"
+}
+
+variable "description" {
+  type        = string
+  description = "(Optional) Description of the function."
+  default     = "" # ${var.name} HTTP Cloud Function"
+}
+
+variable "runtime" {
+  type        = string
+  description = "(Optional) The runtime in which the function is going to run. One of `nodejs6`, `nodejs8`, `nodejs10`, `python37`, `go111`. Default value is `python37`."
+  default     = "python37"
+}
+
+variable "entry_point" {
+  type        = string
+  description = "(Optional) Name of the function that will be executed when the Google Cloud Function is triggered. Default value is `__main__`"
+  default     = "__main__"
+}
+
+variable "available_memory_mb" {
+  type        = number
+  description = "(Optional) Memory (in MB), available to the function. Default value is 128. Allowed values are: 128, 256, 512, 1024, and 2048."
+  default     = 128
+}
+
+variable "timeout" {
+  type        = number
+  description = "(Optional) Timeout (in seconds) for the function. Default value is 60 seconds. Cannot be more than 540 seconds."
+  default     = 60
+}
+
+variable "environment_variables" {
+  type        = map
+  description = "(Optional) A set of key/value environment variable pairs to assign to the function."
+  default     = {}
+}
+
+variable "service_account_email" {
+  type        = string
+  description = "(Optional) If provided, the self-provided service account to run the function with."
+  default     = ""
+}
+
+variable "vpc_connector" {
+  type        = string
+  description = "(Optional) If provided, the vpc connector to use"
+  default     = null
+}
+
+variable "event_type" {
+  type        = string
+  description = "(Optional) If provided, the event to observe. For example providers/cloud.pubsub/eventTypes/topic.publish or providers/cloud.storage/eventTypes/object.change"
+  default     = null
+}
+variable "event_resource" {
+  type        = string
+  description = "(Optional) If provided, the name of the resource from which to observe events. For example, 'myBucket' or 'projects/my-project/topics/my-topic'"
+  default     = null
+}
+
+# @TODO
+
+# region
+# labels
+# max_instances
